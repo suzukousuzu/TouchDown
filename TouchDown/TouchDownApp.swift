9e86940d78ed3068e34426c10f31870cbdc43382
Kota Suzuki <addressuzuki@gmail.com>
2024-02-18 09:10:09 +0900

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by 鈴木航太 on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
